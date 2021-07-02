801272d9c001f71b734de003edc3b383f55ad929
unknown <vmarchitiello59@gmail.com>
2021-07-01 22:50:42 -0300

    ok

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using EPlayersASPNet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPlayersMVC.Controllers
+{
+    [Route("Login")]
+    public class LoginController : Controller
+    {
+
+        [TempData]
+        public string Mensagem {get; set;}
+
+
+        Jogador jogadorModel = new Jogador();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Logar")]
+        public IActionResult Logar(IFormCollection form)
+        {
+           List<string> jogadoresCSV = jogadorModel.LerTodasLinhasCSV("Database/jogador.csv");
+
+            var logado = jogadoresCSV.Find(
+                x =>
+                x.Split(";")[3] == form ["Email"] && 
+                x.Split(";")[4] == form ["Senha"]
+            );
+
+            if (logado != null)
+            {
+                HttpContext.Session.SetString("_UserName", logado.Split(";")[1]);
+                return LocalRedirect ("~/");
+            }
+
+            Mensagem = "Dados incorretos, tente novamente...";
+            return LocalRedirect("~/Login");
+
+        }
+
+        [Route("Logout")]
+        public IActionResult Logout(){
+            HttpContext.Session.Remove("_UserName");
+            return LocalRedirect ("~/");
+        }
+    } 
+}
